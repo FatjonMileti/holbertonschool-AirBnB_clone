2d028ca1bd11e49691fb114bb26977befdbe4442
viviani22 <vivianxhumetiku@gmail.com>
2022-10-14 03:49:26 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,20 +14,17 @@ class FileStorage:
 
     def new(self, obj):
         key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         new_dict = {}
         for key in FileStorage.__objects:
             new_dict[key] = FileStorage.__objects[key].to_dict()
-        with open(self.__file_path, 'w') as f:
+        with open(FileStorage.__file_path, 'w') as f:
             json.dump(new_dict, f)
 
-     
     def reload(self):
-        new_dict = {}
-        if path.exists(self.__file_path):
-            with open(self.__file_path) as f:
-                new_dict = json.load(f)
-                for key, value in new_dict.items():
-                    self.__objects[key] = eval(f"{value['__class__']}")(**value)
+        if path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path) as f:
+                for key, value in (json.load(f)).items():
+                    FileStorage.__objects[key] = eval(f"{value['__class__']}")(**value)
